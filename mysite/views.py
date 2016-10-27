146c28cc49e63190f1d9fd195ea459864dd3ec0a
Tom Victor <vjtomvictor@icloud.com>
2016-10-27 09:31:48 +0530

    Mvc setup done

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.tokens import default_token_generator
+
 
 # Create your views here.
+
+
+def home(request):
+	return render(request, 'index.html', {})
